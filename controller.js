07a310635ba9c00159de92822473d24a3fcd65e2
Kevin Cain <kevinmcain@hotmail.com>
2016-03-03 14:15:59 -0800

    Update controller.js

--- controller.js
+++ controller.js
@@ -38,54 +38,6 @@
 
 				});
 			};
-			
-			
-			
-			$scope.getNewickFromS3Bucket = function() {
-
-				//var AWS = require('aws-sdk');
-				
-				AWS.config.update(
-				  {
-					accessKeyId: "AKIAIPG5APNTFXVOISIA",
-					secretAccessKey: "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",
-					region: 'us-west-2'
-				  }
-				);
-				
-				var s3 = new AWS.S3();
-				var options = {
-					Bucket    : "smx.spark.bio.bucket",
-					Key    : "treeOutput/newick",
-				};
-				
-				var fileStream = s3.getObject(options).createReadStream();
-				
-				var string = ''
-				fileStream.on('readable',function(buffer){
-				  var part = buffer.read().toString();
-				  string += part;
-				  console.log('stream data ' + part);
-				});
-
-
-				fileStream.on('end',function(){
-				 console.log('final output ' + string);
-				 
-				 $scope.newick = data;
-					
-					var dataObject = { newick: $scope.newick };
-
-					phylocanvas = new Smits.PhyloCanvas(
-						dataObject,
-						'svgCanvas',
-						333, 500
-						//,'circular'
-					);
-				 
-				});
-				
-			};	
 		}
 	]);
 	
